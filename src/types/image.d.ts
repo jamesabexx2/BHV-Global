847f375648858acdfb8263df51ac986598892f16
emontes <emontes@dstr.net>
2025-06-07 18:24:12 -0500

    feat: implement client-side views for mission, contact and ungating pages with static export config

---
+++ src/types/image.d.ts
@@ -0,0 +1,26 @@
+declare module '*.jpg' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.jpeg' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.png' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.svg' {
+  import * as React from 'react';
+  export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+}
+
+declare module '*.gif' {
+  const content: string;
+  export default content;
+}
