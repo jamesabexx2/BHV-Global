6e54dca26333792e568445a544ebf88c8ca8c929
emontes <emontes@dstr.net>
2025-06-07 01:22:20 -0500

    refactor: remove unused Link import from Footer component

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import React from 'react';
-import Link from 'next/link';
 import Image from 'next/image';
 import { useLanguage } from '@/contexts/LanguageContext';
 import { Mail } from 'lucide-react';
